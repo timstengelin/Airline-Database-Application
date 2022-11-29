5fa3d18ed066e70a1f372e5eefbf3a27c7144c92
LAPTOP-MVV820R8\timst <Tim.Stengelin@htwg-konstanz.de>
2022-11-29 21:44:22 +0900

    populate database V1

--- populateDatabase.py
+++ populateDatabase.py
@@ -1,4 +1,21 @@
+import mysql.connector
+
+nameDB = 'airlinedatabase'
 
 
 def populateDB():
-    print('TODO')
+    connectToDB()
+
+
+def connectToDB():
+    cnx = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        passwd='root',
+        database=nameDB)
+    cursor = cnx.cursor()
+
+    # TODO
+
+    cursor.close()
+    cnx.close()
